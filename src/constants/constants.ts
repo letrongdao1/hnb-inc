6bdd9399f256ef287e7c278ea5582ba976ea0bd4
letrongdao1 <letrongdaocontact8@gmail.com>
2026-01-12 16:20:06 +0700

    UPLOAD_REQUIRED_SECOND_PER_MB 0.5s to 1s

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -11,7 +11,7 @@ export const GLOBAL_DATE_FORMAT = "DD/MM/YYYY";
 export const DEFAULT_PAGE_SIZE = 20;
 export const DEFAULT_IMAGE_PAGE_SIZE = 50;
 
-export const UPLOAD_REQUIRED_SECOND_PER_MB = 0.5;
+export const UPLOAD_REQUIRED_SECOND_PER_MB = 1;
 
 export const MAX_BANK_ACCOUNT_CAPACITY = 4;
 
